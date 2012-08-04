7bea3c0af6c5eed3a0c8f818e529d69f406e0835
Ben Williams <bwilliams@kirix.com>
2012-08-04 22:12:38 +0000

    moved more kl/string.h functions into kl/string.cpp

--- kl/include/kl/string.h
+++ kl/include/kl/string.h
@@ -39,75 +39,10 @@ namespace kl
 {
 
 
-inline void trimRight(std::string& s)
-{
-    size_t orig_len = s.length();
-    size_t len = orig_len;
-
-    while (len > 0)
-    {
-        if (!isspace((unsigned char)s[len-1]))
-            break;
-        --len;
-    }
-
-    if (orig_len != len)
-    {
-        s.resize(len);
-    }
-}
-
-inline void trimRight(std::wstring& s)
-{
-    size_t orig_len = s.length();
-    size_t len = orig_len;
-
-    while (len > 0)
-    {
-        if (!iswspace(s[len-1]))
-            break;
-        --len;
-    }
-
-    if (orig_len != len)
-    {
-        s.resize(len);
-    }
-}
-
-inline void trimLeft(std::string& s)
-{
-    const char* c = s.c_str();
-    int cnt = 0;
-
-    while (*c && isspace((unsigned char)*c))
-    {
-        ++cnt;
-        ++c;
-    }
-
-    if (cnt > 0)
-    {
-        s.erase(0, cnt);
-    }
-}
-
-inline void trimLeft(std::wstring& s)
-{
-    const wchar_t* c = s.c_str();
-    int cnt = 0;
-
-    while (*c && iswspace(*c))
-    {
-        ++cnt;
-        ++c;
-    }
-
-    if (cnt > 0)
-    {
-        s.erase(0, cnt);
-    }
-}
+void trimRight(std::string& s);
+void trimRight(std::wstring& s);
+void trimLeft(std::string& s);
+void trimLeft(std::wstring& s);
 
 inline void trim(std::string& s)
 {
@@ -241,199 +176,29 @@ inline std::wstring afterLast(const std::wstring& str, wchar_t ch)
     return str.substr(pos+1);
 }
 
+std::wstring itowstring(int val);
 
-inline size_t replaceStr(std::wstring& str,
+
+size_t replaceStr(std::wstring& str,
                          const std::wstring& search,
                          const std::wstring& replace,
-                         bool global = true)
-{
-    size_t offset = 0;
-    size_t search_len = search.length();
-    size_t replace_len = replace.length();
-    size_t num_replaces = 0;
-    
-    while (1)
-    {
-        size_t pos = str.find(search, offset);
-        if (pos == str.npos)
-            break;
-            
-        str.erase(pos, search_len);
-        str.insert(pos, replace);
-        
-        offset = (pos+replace_len);
-        num_replaces++;
-        
-        if (search_len == 0 || !global)
-            break;
-    }
-    
-    return num_replaces;
-}
+                         bool global = true);
 
 
-inline size_t replaceStrNoCase(std::wstring& str,
-                               const std::wstring& search,
-                               const std::wstring& replace)
-{
-    size_t offset = 0;
-    size_t search_len = search.length();
-    size_t replace_len = replace.length();
-    size_t num_replaces = 0;
-    
-    std::wstring uc_search = search;
-    std::wstring uc_str;
-    kl::makeUpper(uc_search);
-    
-    while (1)
-    {
-        uc_str = str;
-        kl::makeUpper(uc_str);
-        size_t pos = uc_str.find(uc_search, offset);
+size_t replaceStrNoCase(std::wstring& str,
+                        const std::wstring& search,
+                        const std::wstring& replace);
 
-        if (pos == str.npos)
-            break;
-            
-        str.erase(pos, search_len);
-        str.insert(pos, replace);
-        
-        offset = pos+replace_len;
-        num_replaces++;
-    }
-    
-    return num_replaces;
-}
-
-
-inline double nolocale_atof(const char* num)
-{
-    static char search_ch = 0;
-    static char replace_ch = 0;
-
-    if (!search_ch)
-    {
-        lconv* l = localeconv();
-        replace_ch = (unsigned char)*l->decimal_point;
-        if (replace_ch == ',')
-        {
-            search_ch = '.';
-        }
-         else
-        {
-            search_ch = ',';
-        }
-    }
-
-    char buf[128];
-    int i = 0;
-
-    while (1)
-    {
-        if (i == sizeof(buf)-1)
-        {
-            buf[i] = 0;
-            break;
-        }
-
-        if (*num == search_ch)
-            buf[i++] = replace_ch;
-             else
-            buf[i++] = *num;
-
-        if (*num == 0)
-            break;
-        num++;
-    }
-
-    return atof(buf);
-}
-
+double nolocale_atof(const char* num);
 
 inline double nolocale_atof(const std::string& num)
 {
     return nolocale_atof(num.c_str());
 }
 
-inline int wtoi(const wchar_t* s)
-{
-    char buf[64];
-    int i = 0;
-
-    while (*s)
-    {
-        buf[i] = (char)*s;
-        ++s;
-        if (i > 60)
-            break;
-        ++i;
-    }
-
-    buf[i] = 0;
-
-    return atoi(buf);
-}
-
-inline double wtof(const wchar_t* s)
-{
-    char buf[64];
-    int i = 0;
-
-    while (*s)
-    {
-        buf[i] = (char)*s;
-        ++s;
-        if (i > 60)
-            break;
-        ++i;
-    }
-
-    buf[i] = 0;
-
-    return atof(buf);
-}
-
-inline double nolocale_wtof(const wchar_t* num)
-{
-    static wchar_t search_ch = 0;
-    static wchar_t replace_ch = 0;
-
-    if (!search_ch)
-    {
-        lconv* l = localeconv();
-        replace_ch = (unsigned char)*l->decimal_point;
-        if (replace_ch == L',')
-        {
-            search_ch = L'.';
-        }
-         else
-        {
-            search_ch = L',';
-        }
-    }
-
-    char buf[128];
-    int i = 0;
-
-    while (1)
-    {
-        if (i == sizeof(buf)-1)
-        {
-            buf[i] = 0;
-            break;
-        }
-
-        if (*num == search_ch)
-            buf[i++] = (char)replace_ch;
-             else
-            buf[i++] = (char)*num;
-
-        if (*num == 0)
-            break;
-        num++;
-    }
-
-    return atof(buf);
-}
+int wtoi(const wchar_t* s);
+double wtof(const wchar_t* s);
+double nolocale_wtof(const wchar_t* num);
 
 inline double nolocale_wtof(const std::wstring& num)
 {
@@ -563,174 +328,29 @@ inline void towstring(std::wstring& dest, const char* src, size_t len = (size_t)
 // these functions convert a std::(w)string to a UCS-2 buffer and vice versa.
 // Big-endian and little-endian versions are provided
 
-inline void string2ucsle(unsigned char* dest,
-                         const std::string& src,
-                         int pad_len)
-{
-    size_t i = 0;
-    size_t str_len = src.length();
-    wchar_t ch;
+void string2ucsle(unsigned char* dest,
+                  const std::string& src,
+                  int pad_len);
 
-    while (pad_len)
-    {
-        if (i < str_len)
-            ch = (unsigned char)src[i];
-             else
-            ch = 0;
+void wstring2ucsle(unsigned char* dest,
+                   const std::wstring& src,
+                   int pad_len);
 
-        *dest = (ch & 0xff);
-        ++dest;
-        *dest = (ch >> 8) & 0xff;
-        ++dest;
-        ++i;
-        --pad_len;
-    }
-}
+void ucsle2wstring(std::wstring& dest,
+                   const unsigned char* src,
+                   int buf_len);
 
+void ucsle2string(std::string& dest,
+                  const unsigned char* src,
+                  int buf_len);
 
-inline void wstring2ucsle(unsigned char* dest,
-                          const std::wstring& src,
-                          int pad_len)
-{
-    size_t i = 0;
-    size_t str_len = src.length();
-    wchar_t ch;
+void wstring2ucsbe(unsigned char* dest,
+                   const std::wstring& src,
+                   int pad_len);
 
-    while (pad_len)
-    {
-        if (i < str_len)
-            ch = src[i];
-             else
-            ch = 0;
-
-        *dest = (ch & 0xff);
-        ++dest;
-        *dest = (ch >> 8) & 0xff;
-        ++dest;
-        ++i;
-        --pad_len;
-    }
-}
-
-inline void ucsle2wstring(std::wstring& dest,
-                        const unsigned char* src,
-                        int buf_len)
-{
-#ifdef WIN32
-    const wchar_t* wsrc = (const wchar_t*)src;
-    int len;
-    for (len = 0; len < buf_len; ++len)
-    {
-        if (!*(wsrc+len))
-            break;
-    }
-
-    dest.assign(wsrc, len);
-#else
-    int i;
-    wchar_t ch;
-
-    dest.resize(buf_len);
-    for (i = 0; i < buf_len; ++i)
-    {
-        ch = (wchar_t)*src;
-        ++src;
-        ch |= (((wchar_t)*src) << 8);
-        ++src;
-
-        if (ch == 0)
-        {
-            dest.resize(i);
-            break;
-        }
-
-        dest[i] = ch;
-    }
-#endif
-}
-
-inline void ucsle2string(std::string& dest,
-                         const unsigned char* src,
-                         int buf_len)
-{
-    int i;
-    wchar_t ch;
-
-    dest.resize(buf_len);
-    for (i = 0; i < buf_len; ++i)
-    {
-        ch = (wchar_t)*src;
-        ++src;
-        ch |= (((wchar_t)*src) << 8);
-        ++src;
-
-        if (ch == 0)
-        {
-            dest.resize(i);
-            break;
-        }
-
-        if (ch <= 255)
-        {
-            dest[i] = (char)(unsigned char)ch;
-        } 
-         else
-        {
-            dest[i] = '?';
-        }
-    }
-}
-
-
-inline void wstring2ucsbe(unsigned char* dest,
-                          const std::wstring& src,
-                          int pad_len)
-{
-    size_t i = 0;
-    size_t str_len = src.length();
-    wchar_t ch;
-
-    while (pad_len)
-    {
-        if (i < str_len)
-            ch = src[i];
-             else
-            ch = 0;
-
-        *dest = ((ch >> 8) & 0xff);
-        ++dest;
-        *dest = ch & 0xff;
-        ++dest;
-        ++i;
-        --pad_len;
-    }
-}
-
-inline void ucsbe2wstring(std::wstring& dest,
-                        const unsigned char* src,
-                        int buf_len)
-{
-    int i;
-    wchar_t ch;
-
-    dest.resize(buf_len);
-    for (i = 0; i < buf_len; ++i)
-    {
-        ch = ((wchar_t)*src) << 8;
-        ++src;
-        ch |= (wchar_t)*src;
-        ++src;
-
-        if (ch == 0)
-        {
-            dest.resize(i);
-            break;
-        }
-
-        dest[i] = ch;
-    }
-}
-
+void ucsbe2wstring(std::wstring& dest,
+                   const unsigned char* src,
+                   int buf_len);
 
 class tstr
 {
@@ -832,4 +452,3 @@ struct cmp_nocase : std::binary_function<const std::wstring&,
 
 
 #endif
-
