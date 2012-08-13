df47834d1a0b7b6e75b354e9cf6ddaa94043e13a
Ben Williams <bwilliams@kirix.com>
2012-08-13 18:25:03 +0000

    added xdclient.h

---
+++ tango/xdclient/xdclient.h
@@ -0,0 +1,24 @@
+
+#include <kl/klib.h>
+#include "../../kscript/kscript.h"
+#include "../../kscript/json.h"
+
+namespace kscript { class JsonNode; }
+typedef kscript::JsonNode JsonNode;
+
+#include "tango.h"
+
+#include "../xdcommon/xdcommon.h"
+#include "../xdcommon/errorinfo.h"
+#include "../xdcommon/dbattr.h"
+#include "../xdcommon/fileinfo.h"
+#include "../xdcommon/sqlcommon.h"
+#include "../xdcommon/util.h"
+
+#include "database.h"
+#include "set.h"
+#include "iterator.h"
+
+
+
+
