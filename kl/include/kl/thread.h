731c5ce21063446889daed5ed12ef03b4dc2b9f5
Aaron Williams <awilliams@kirix.com>
2012-11-14 22:14:44 +0000

    added kl/thread.*

---
+++ kl/include/kl/thread.h
@@ -0,0 +1,22 @@
+/*!
+ *
+ * Copyright (c) 2012, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  Standard Library
+ * Author:   Aaron L. Williams
+ * Created:  2012-11-14
+ *
+ */
+
+
+#ifndef __KL_THREAD_H
+#define __KL_THREAD_H
+
+
+namespace kl
+{
+};
+
+
+#endif
+
---
+++ kl/src/thread.cpp
@@ -0,0 +1,18 @@
+/*!
+ *
+ * Copyright (c) 2012, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  Standard Library
+ * Author:   Aaron L. Williams
+ * Created:  2012-11-14
+ *
+ */
+
+
+#include "kl/thread.h"
+
+
+namespace kl
+{
+};
+
