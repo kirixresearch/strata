f284ff7ccc003bb2344e0e9418866bf5aacb6aa7
Ben Williams <bwilliams@kirix.com>
2012-11-27 09:10:33 +0000

    renamed jobsint.*

---
+++ jobs/jobspch.cpp
@@ -0,0 +1,13 @@
+/*!
+ *
+ * Copyright (c) 2012, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  Job Automation Library
+ * Author:   Benjamin I. Williams
+ * Created:  2012-11-26
+ *
+ */
+
+
+#include "jobsint.h"
+
