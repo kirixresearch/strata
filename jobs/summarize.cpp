b0dc61e5b1632103ecaa4236e9be822cb85df769
Ben Williams <bwilliams@kirix.com>
2013-04-01 22:41:43 +0000

    remove reference to openSet()

--- jobs/summarize.cpp
+++ jobs/summarize.cpp
@@ -107,9 +107,8 @@ int SummarizeJob::runJob()
         summary_columns.push_back(col);
     }
 
-    tango::ISetPtr input_set = m_db->openSet(input_path);
     tango::IStructurePtr input_structure = m_db->describeTable(input_path);
-    if (input_set.isNull() || input_structure.isNull())
+    if (input_structure.isNull())
     {
         m_job_info->setState(jobStateFailed);
         return 0;
