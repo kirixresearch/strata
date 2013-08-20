6250706044230d161e4104f1a03288ff8b14429f
Ben Williams <bwilliams@kirix.com>
2013-08-20 21:28:24 +0000

    changed port

--- sdserv/proxy.cpp
+++ sdserv/proxy.cpp
@@ -34,7 +34,7 @@ static void* request_callback(enum mg_event evt,
 
         int sock;
         char* ipaddress = "127.0.0.1";
-        int port = 80;
+        int port = 4800;
         struct sockaddr_in serveraddr;
         std::string request;
 
