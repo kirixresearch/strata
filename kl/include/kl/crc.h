9a1b7a7efab4cb0c777eabf96171ffd4da7d5ea5
Ben Williams <bwilliams@kirix.com>
2013-10-29 13:13:09 +0000

    added crc routines to the kl library

---
+++ kl/include/kl/crc.h
@@ -0,0 +1,29 @@
+/*!
+ *
+ * Copyright (c) 2006-2013, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  Standard Library
+ * Author:   Benjamin I. Williams
+ * Created:  2013-10-29
+ *
+ */
+
+
+#ifndef __KL_CRC_H
+#define __KL_CRC_H
+
+
+
+namespace kl
+{
+
+unsigned short crc16(const unsigned char* data, unsigned int size, unsigned short crc = 0);
+unsigned long crc32(const unsigned char* data, int size);
+
+
+};
+
+
+
+#endif
+
