e69b20fc504721b5a70d8d83d6c118f2027d5479
Ben Williams <bwilliams@kirix.com>
2013-11-15 20:24:48 +0000

    added websockets client stub

---
+++ sdserv/websockets.cpp
@@ -0,0 +1,24 @@
+/*!
+ *
+ * Copyright (c) 2012-2013, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  SD Server
+ * Author:   Benjamin I. Williams
+ * Created:  2013-11-15
+ *
+ */
+
+
+#include "sdserv.h"
+#include "request.h"
+#include "controller.h"
+
+
+
+
+void WebSocketsClient::run()
+{
+
+}
+
+
---
+++ sdserv/websockets.h
@@ -0,0 +1,29 @@
+/*!
+ *
+ * Copyright (c) 2012-2013, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  SD Server
+ * Author:   Benjamin I. Williams
+ * Created:  2013-11-15
+ *
+ */
+
+ 
+#ifndef __SDSERV_SDSERV_H
+#define __SDSERV_SDSERV_H
+
+
+
+
+
+class WebSocketsClient
+{
+public:
+
+    void run();
+
+};
+
+
+
+#endif
