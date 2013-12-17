c1be472942aff37a85241cefd4d27053377fbd82
Ben Williams <bwilliams@kirix.com>
2013-12-17 23:55:47 +0000

    setup a basic test which lists a sdserv directory

--- sdservtests/test0001.js
+++ sdservtests/test0001.js
@@ -1,8 +1,28 @@
 
 tests.push(function() {
 
+	var passed = false;
 
+    $.ajax({
+                url: g_url,
+				type: 'POST',
+				data: {
+				},
+                async: false,
+                success: function (data)
+				{
+					// do test of data here
+					var obj = JSON.parse(data);
+					
+					if (obj.success)
+						passed = true;
+				},
+				error: function ()
+				{
+					alert("AJAX error");
+					passed = false;
+				}
+            });
 
-
-    return true; // passed
+    return passed; // passed
 });
