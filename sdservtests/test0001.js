371512c57fe0d9ccf92ffe2bb39c2b6643669271
Aaron Williams <awilliams@kirix.com>
2013-12-18 22:32:24 +0000

    added util.js; converted tasb to spaces

--- sdservtests/test0001.js
+++ sdservtests/test0001.js
@@ -1,27 +1,28 @@
 
 tests.push(function() {
 
-	var passed = false;
+    var passed = false;
 
     $.ajax({
                 url: g_url,
-				type: 'POST',
-				data: {
-				},
+                type: 'POST',
+                data: {
+                },
                 async: false,
                 success: function (data)
-				{
-					// do test of data here
-					var obj = JSON.parse(data);
-					
-					if (obj.success)
-						passed = true;
-				},
-				error: function ()
-				{
-					alert("AJAX error");
-					passed = false;
-				}
+                {
+log(data);
+                    // do test of data here
+                    var obj = JSON.parse(data);
+                    
+                    if (obj.success)
+                        passed = true;
+                },
+                error: function ()
+                {
+                    alert("AJAX error");
+                    passed = false;
+                }
             });
 
     return passed; // passed
