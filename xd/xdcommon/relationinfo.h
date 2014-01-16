63c8a9d429d46de016bade38954286c9c6afb138
Ben Williams <bwilliams@kirix.com>
2014-01-16 17:28:05 +0000

    added RelationInfo to xdcommon

---
+++ xd/xdcommon/relationinfo.h
@@ -0,0 +1,108 @@
+/*!
+ *
+ * Copyright (c) 2014, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  XD Database Library
+ * Author:   Benjamin I. Williams
+ * Created:  2014-01-16
+ *
+ */
+
+
+#ifndef __XDCOMMON_RELATIONINFO_H
+#define __XDCOMMON_RELATIONINFO_H
+
+
+class RelationInfo : public xd::IRelation
+{
+    XCM_CLASS_NAME("xd.RelationInfo")
+    XCM_BEGIN_INTERFACE_MAP(RelationInfo)
+        XCM_INTERFACE_ENTRY(xd::IRelation)
+    XCM_END_INTERFACE_MAP()
+
+public:
+
+    RelationInfo()
+    {
+    }
+
+    void setRelationId(const std::wstring& new_val)
+    {
+        m_relation_id = new_val;
+    }
+
+    const std::wstring& getRelationId()
+    {
+        return m_relation_id;
+    }
+
+    void setTag(const std::wstring& new_val)
+    {
+        m_tag = new_val;
+    }
+
+    const std::wstring& getTag()
+    {
+        return m_tag;
+    }
+
+    void setLeftSet(const std::wstring& new_value)
+    {
+        m_left_path = new_value;
+        m_left_setid = L"";
+    }
+    
+    std::wstring getLeftTable()
+    {
+        return m_left_path;
+    }
+
+    void setLeftExpression(const std::wstring& new_value)
+    {
+        m_left_expression = new_value;
+    }
+
+    std::wstring getLeftExpression()
+    {
+        return m_left_expression;
+    }
+
+
+    
+    void setRightSet(const std::wstring& new_value)
+    {
+        m_right_path = new_value;
+        m_right_setid = L"";
+    }
+
+
+    std::wstring getRightTable()
+    {
+        return m_right_path;
+    }
+
+    void setRightExpression(const std::wstring& new_value)
+    {
+        m_right_expression = new_value;
+    }
+
+    std::wstring getRightExpression()
+    {
+        return m_right_expression;
+    }
+
+private:
+
+    std::wstring m_relation_id;
+    std::wstring m_tag;
+
+    std::wstring m_left_path;
+    std::wstring m_right_path;
+    std::wstring m_left_expression;
+    std::wstring m_right_expression;
+};
+
+
+
+#endif
+
