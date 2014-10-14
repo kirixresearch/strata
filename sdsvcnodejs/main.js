cddbbdf8bc5f6abe20be818bc49edc036ff59ee3
Ben Williams <bwilliams@kirix.com>
2014-10-14 13:28:11 +0000

    switched proxy to bouncy; path striping

--- sdsvcnodejs/main.js
+++ sdsvcnodejs/main.js
@@ -2,6 +2,7 @@
 
 var http = require('http');
 var http_proxy = require('http-proxy');
+var bouncy = require('bouncy');
 var url = require('url');
 var process = require('child_process');
 var g_sdserv_cmd = "D:\\build32\\src\\trunk\\releaseu\\sdserv.exe";
@@ -50,7 +51,7 @@ function fetchPort(group, callback, error)
         var cmdline = g_sdserv_cmd + " -d " + group + " -p " + info.port + " --idle-quit 3000 ";
         
         var cmd = "D:\\build32\\src\\trunk\\releaseu\\sdserv.exe";
-        var args = [ "-d", group, "-p", info.port, "--idle-quit", "3000" ];
+        var args = [ "-d", group, "-p", info.port, "-s", 1 /* strip off first part of url */, "--idle-quit", "3000" ];
 
         
         //var cmd = "c:\\Program Files\\nodejs\\node.exe";
@@ -85,6 +86,66 @@ function fetchPort(group, callback, error)
 }
 
 
+
+
+var bouncy = require('bouncy');
+
+var server = bouncy(function (request, response, bounce)
+{
+    var url_parsed = url.parse(request.url,true);
+    var url_parts = url_parsed.pathname.split('/');
+	
+	var group = '';
+    if (url_parts.length > 0)
+		group = url_parts[1];
+	console.log(JSON.stringify(url_parts));
+	if (group.length == 0 || group.substr(0,7).toLowerCase() == 'favicon')
+	{
+		response.statusCode = 404;
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.end('{ "success": false, "msg": "Not found" }');
+		return;
+	}
+	
+		
+	console.log(new Date().toISOString() + " - " + request.url + " HTTP " + request.httpVersion);
+	console.log(JSON.stringify(request.headers,null,4));
+	
+/*
+	var question = request.url.indexOf('?');
+	var new_url;
+	
+	if (question == -1)
+		new_url = request.url;
+		 else
+		new_url = request.url.substr(0, question);
+	
+	var slash = new_url.indexOf('/',1);
+	if (slash == -1)
+		new_url = '/';
+		 else
+		new_url = new_url.substr(slash);
+	if (question != -1)
+		new_url += request.url.substr(question);
+*/
+
+	fetchPort(group, function(port) {
+		console.log("forwarding to http://localhost:" + port);
+		bounce(port);
+	});
+
+	/*
+    else {
+		// error
+        res.statusCode = 404;
+        res.end('no such host');
+    }
+	*/
+});
+server.listen(80);
+
+
+/*
 var proxy = http_proxy.createProxyServer({agent:false});
 
 
@@ -137,7 +198,7 @@ var http_callback = function (request, response) {
 var server = http.createServer(http_callback);
 server.listen(80);
 
-
+*/
 
 
 
