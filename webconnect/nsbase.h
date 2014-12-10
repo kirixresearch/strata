20cd1bdfdd716ddefa8c9d4739f6d31fa7822639
Ben Williams <bwilliams@kirix.com>
2014-12-10 11:49:10 +0000

    msvc compile fix

--- webconnect/nsbase.h
+++ webconnect/nsbase.h
@@ -42,7 +42,7 @@ typedef long long int64_t;
 typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef short int int16_t;
-typedef short int char16_t;
+//typedef short int char16_t;
 typedef unsigned short int uint16_t;
 typedef unsigned char uint8_t;
 typedef signed char int8_t;
