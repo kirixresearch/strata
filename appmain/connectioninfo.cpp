33eae179128f6fa421cf57bfeb8dabaa3f39cbd6
Ben Williams <ben@flex.io>
2019-02-21 13:13:00 +0100

    added missing connectioninfo.cpp

---
+++ appmain/connectioninfo.cpp
@@ -0,0 +1,12 @@
+/*!
+ *
+ * Copyright (c) 2005-2013, Kirix Research, LLC.  All rights reserved.
+ *
+ * Project:  Application Client
+ * Author:   Benjamin I. Williams
+ * Created:  2019-02-19
+ *
+ */
+
+
+#include "appmain.h"
