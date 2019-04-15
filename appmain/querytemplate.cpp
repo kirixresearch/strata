fc42e2ea157b9baf4d3a0fae8c6e580a3b43a15f
Ben Williams <bwilliams@kirix.com>
2019-04-16 00:48:04 +0200

    fix wrong migration to isSamePath()

--- appmain/querytemplate.cpp
+++ appmain/querytemplate.cpp
@@ -256,7 +256,7 @@ wxString QueryTemplate::getQueryString()
         for (join_it = all_joins.begin();
              join_it != all_joins.end(); ++join_it)
         {
-            if (isSamePath(towstr(join_it->right_path), towstr(tbl_it->path)) == 0)
+            if (isSamePath(towstr(join_it->right_path), towstr(tbl_it->path)))
             {
                 found = true;
                 break;
