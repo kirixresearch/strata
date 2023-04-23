1f8fe2075532a10d2e0b0d71d2ee4fa40e3df517
Benjamin Williams <bwilliams@fopen.org>
2023-04-23 23:21:26 +0200

    removed unnecessary ;

--- xd/xdfs/ttbfile.cpp
+++ xd/xdfs/ttbfile.cpp
@@ -229,7 +229,7 @@ bool TtbTable::create(const std::wstring& filename, const std::vector<xd::Column
             case xd::typeBoolean:  col_width = 1; col_scale = 0; break;
             case xd::typeDateTime: col_width = 8; col_scale = 0; break;
             default:                  break;
-        };
+        }
         
         if (nulls_allowed)
         {
