3afb984ed13be3cb1839f1e46271a456f9809df5
Benjamin Williams <bwilliams@fopen.org>
2023-06-18 21:29:58 +0200

    upgraded zlib supp

--- supp/make_zlib.bat
+++ supp/make_zlib.bat
@@ -11,8 +11,21 @@ unzip zip\zlib.zip
 
 cd zlib
 
-nmake -f win32\Makefile.msc clean all
 nmake -f win32\Makefile_Debug.msc clean all
+mkdir Debug
+copy *.lib Debug
+copy *.dll Debug
+copy *.pdb Debug
+
+nmake -f win32\Makefile.msc clean all
+mkdir Release
+copy *.lib Release
+copy *.dll Release
+copy *.pdb Release
+
+erase *.lib /f
+erase *.pdb /f
+erase *.dll /f
 
 popd
 
