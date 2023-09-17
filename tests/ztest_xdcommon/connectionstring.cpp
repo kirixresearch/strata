23199c2d65546e66c3426004a6c88d3a50b29f0c
Benjamin Williams <bwilliams@fopen.org>
2023-09-17 21:59:57 +0200

    renamed tests and added new connectionstring file

---
+++ tests/ztest_xdcommon/connectionstring.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../xd/xdcommon/localrowcache2.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ztestxdcommon
+{
+	TEST_CLASS(connectionstring)
+	{
+	public:
+		
+		TEST_METHOD(TestLocalRowValueSetGetData2)
+		{
+
+			Assert::AreEqual(0, 0);
+		}
+
+	};
+}
