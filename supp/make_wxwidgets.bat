f7c0f6f421a6b2eb77441a3545e5dcc26bb74ecc
Benjamin Williams <bwilliams@fopen.org>
2024-01-24 13:52:13 +0100

    added edge web browser functionality to make_wxwidgets

--- supp/make_wxwidgets.bat
+++ supp/make_wxwidgets.bat
@@ -8,9 +8,13 @@ if "%FrameworkDir%"=="" (
 pushd "%~dp0"
 rmdir wxWidgets /s /q
 unzip zip\wxwidgets.zip
+unzip zip\microsoft.web.webview2.1.0.2210.55.nupkg -d wxWidgets\3rdparty\webview2
 
-cd wxWidgets\build\msw
-
+pushd wxWidgets\include\wx\msw
+cscript %~dp0\util\replace.vbs setup.h "wxUSE_WEBVIEW_IE 1" "wxUSE_WEBVIEW_IE 0"
+cscript %~dp0\util\replace.vbs setup.h "wxUSE_WEBVIEW_EDGE 0" "wxUSE_WEBVIEW_EDGE 1"
+cscript %~dp0\util\replace.vbs setup.h "wxUSE_WEBVIEW_EDGE_STATIC 0" "wxUSE_WEBVIEW_EDGE_STATIC 1"
+popd
 
 if "%PLATFORM%"=="x64" goto x64
 :x86
@@ -21,8 +25,10 @@ SET MSBUILD_PLATFORM=x64
 goto done
 :done
 
-msbuild wx_vc16.sln /t:Rebuild /p:Configuration=Debug /p:Platform=%MSBUILD_PLATFORM%
-msbuild wx_vc16.sln /t:Rebuild /p:Configuration=Release /p:Platform=%MSBUILD_PLATFORM%
+cd wxWidgets\build\msw
+
+msbuild wx_vc17.sln /t:Rebuild /p:Configuration=Debug /p:Platform=%MSBUILD_PLATFORM%
+msbuild wx_vc17.sln /t:Rebuild /p:Configuration=Release /p:Platform=%MSBUILD_PLATFORM%
 
 popd
 
