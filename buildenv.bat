145782fa43988834abd2dd773a9333655d1243da
Benjamin Williams <bwilliams@fopen.org>
2025-01-30 20:42:12 +0100

    updated postgres build scripts

---
+++ buildenv.bat
@@ -0,0 +1,23 @@
+@echo off
+set "SCRIPT_DIR=%~dp0"
+
+if not exist "%SCRIPT_DIR%supp\setup_supp.bat" (
+    echo Error: setup_supp.bat not found.
+    echo Please run: cd supp ^&^& setup_supp.bat
+    exit /b 1
+)
+
+if "%1"=="x64" (
+    title x64 Build Environment
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" && call "%SCRIPT_DIR%postgres/util/perl/portableshell.bat"
+) else if "%1"=="x86" (
+    title x86 Build Environment
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat" && call "%SCRIPT_DIR%postgres/util/perl/portableshell.bat"
+) else (
+    echo Please specify architecture: x64 or x86
+    exit /b 1
+)
+
+call "%SCRIPT_DIR%supp/util/perl/portableshell.bat"
+
+set SUPP_ENV=true
---
+++ supp/download.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set "SCRIPT_DIR=%~dp0"
+set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+if not exist "%SCRIPT_DIR%\zip\strawberry-perl-5.40.0.1-64bit-portable.zip" (
+    curl -L -o "%SCRIPT_DIR%\zip\strawberry-perl-5.40.0.1-64bit-portable.zip" https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54001_64bit_UCRT/strawberry-perl-5.40.0.1-64bit-portable.zip
+)
+
+if not exist "%SCRIPT_DIR%\zip\postgres-16.6.zip" (
+    curl -L -o "%SCRIPT_DIR%\zip\postgres-16.6.zip" https://github.com/postgres/postgres/archive/refs/tags/REL_16_6.zip
+)
+
