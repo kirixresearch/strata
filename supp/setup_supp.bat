689f07020ee6f4464fcab4bbf589aa45c5c94d67
Benjamin Williams <bwilliams@fopen.org>
2025-11-20 09:48:47 +0100

    fix unzip path

--- supp/setup_supp.bat
+++ supp/setup_supp.bat
@@ -6,13 +6,13 @@ call "%SCRIPT_DIR%download.bat"
 pushd "%~dp0\util"
 rmdir cmakewin32 /s/q 2>nul
 rmdir perlwin32 /s/q 2>nul
-..\..\unzip cmakewin32.zip
+%~dp0\util\unzip cmakewin32.zip
 
 rmdir perl /s/q
 mkdir perl
 pushd perl
-rem ..\..\..\unzip ..\..\zip\strawberry-perl-5.40.0.1-64bit-portable.zip
-..\..\..\unzip ..\..\zip\strawberry-perl-5.38.2.2-64bit-portable.zip
+rem %~dp0\util\unzip ..\..\zip\strawberry-perl-5.40.0.1-64bit-portable.zip
+%~dp0\util\unzip ..\..\zip\strawberry-perl-5.38.2.2-64bit-portable.zip
 popd
 
 popd
